400776181432c633bf1544d2eecc7cea722880a5
tomlukrprogram <tomlukeprogramming@outlook.com>
2023-11-28 02:23:25 +0000

    basic structure of board next, pieces are made

---
+++ src/Components/Game/Game.jsx
@@ -0,0 +1,1 @@
+// import GamePiecesData from "./GamePiecesData";
--- src/Components/Game/GamePiecesData.js
+++ src/Components/Game/GamePiecesData.js
@@ -1,7 +1,62 @@
 
-// GAME PIECES
+const GamePiecesData = {
+pieces:[
+  {
+    type: 'I',
+    shape:[
+    [1,1,1,1],
+    ],
+    color: 'cyan'
+  },
+  {
+    type: 'J',
+    shape:[
+    [1, 0, 0],  
+    [1, 1, 1],
+    ],
+    color: 'blue'
+  },
+  {
+  type: 'L',
+  shape:[
+  [1, 1, 1],  
+  [1, 0, 0],
+  ],
+  color: 'yellow'
+},
+{
+  type: 'T',
+  shape:[
+  [0, 1, 0],  
+  [1, 1, 1],
+  ],
+  color: 'green'
+},
+{
+  type: '0',
+  shape:[
+  [1, 1],  
+  [1, 1],
+  ],
+  color: 'red'
+},
+{
+  type: 'S',
+  shape:[
+  [0, 1, 1],  
+  [1, 1, 0],
+  ],
+  color: 'purple'
+},
+{
+  type: 'Z',
+  shape:[
+  [1, 1, 0],  
+  [0, 1, 1],
+  ],
+  color: 'orange'
+}
+]
 
-/*
-pieces = L, J, T, I, Z, S, O
-*/ 
- 
+}
+export default GamePiecesData
